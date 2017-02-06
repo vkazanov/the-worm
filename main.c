dd35f909d6c7ecc22d5781b7929cc375b7699040
Vladimir Kazanov <vladimir.kazanov@corp.badoo.com>
2017-02-06 09:56:19 +0000

    put a char

--- main.c
+++ main.c
@@ -7,12 +7,13 @@
 int main(int argc, char *argv[])
 {
     (void) argc; (void) argv;
-    TCOD_console_init_root(80, 40, "The Worm", false, TCOD_RENDERER_SDL);
+    TCOD_console_init_root(80, 50, "The Worm", false, TCOD_RENDERER_SDL);
     while (!TCOD_console_is_window_closed()) {
         puts("Checking...!");
         TCOD_key_t key;
         TCOD_sys_check_for_event(TCOD_EVENT_KEY_PRESS, &key, NULL);
         TCOD_console_clear(NULL);
+        TCOD_console_put_char(NULL, 40, 25, '@', TCOD_BKGND_DEFAULT);
         TCOD_console_flush();
     }
 
