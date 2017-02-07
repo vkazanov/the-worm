46779e013d24990d8b9b101ec6a4377f5b52c5a0
Vladimir Kazanov <vladimir.kazanov@corp.badoo.com>
2017-02-07 08:36:10 +0000

    Moving

--- main.c
+++ main.c
@@ -7,16 +7,30 @@
 int main(int argc, char *argv[])
 {
     (void) argc; (void) argv;
+    uint16 player_x = 40, player_y = 25;
     TCOD_console_init_root(80, 50, "The Worm", false, TCOD_RENDERER_SDL);
     while (!TCOD_console_is_window_closed()) {
-        puts("Checking...!");
         TCOD_key_t key;
         TCOD_sys_check_for_event(TCOD_EVENT_KEY_PRESS, &key, NULL);
+        switch(key.vk) {
+        case TCODK_UP:
+            player_y--;
+            break;
+        case TCODK_DOWN:
+            player_y++;
+            break;
+        case TCODK_LEFT:
+            player_x--;
+            break;
+        case TCODK_RIGHT:
+            player_x++;
+            break;
+        default:
+            break;
+        }
         TCOD_console_clear(NULL);
-        TCOD_console_put_char(NULL, 40, 25, '@', TCOD_BKGND_DEFAULT);
+        TCOD_console_put_char(NULL, player_x, player_y, '@', TCOD_BKGND_DEFAULT);
         TCOD_console_flush();
     }
-
-    puts("Hello, world!");
     return EXIT_SUCCESS;
 }
