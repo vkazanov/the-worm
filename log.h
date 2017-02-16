b7544c9511b1141a66af4f22e4f27846f2313ee9
Vladimir Kazanov <vladimir.kazanov@corp.badoo.com>
2017-02-16 10:02:32 +0000

    oops, invalid function attribute

--- log.h
+++ log.h
@@ -2,6 +2,6 @@
 
 void log_init(int w, int h);
 
-void log_msg(const char *message, ...) __attribute__ ((format (printf, 1, 0)));
+void log_msg(const char *message, ...) __attribute__ ((format (printf, 1, 2)));
 
 void log_draw(void);
