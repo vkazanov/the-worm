79035c7e66373600007130dc23b1f6f48b94ae13
Vladimir Kazanov <vladimir.kazanov@corp.badoo.com>
2017-03-06 08:46:54 +0000

    Refactor monster_act

--- monster.c
+++ monster.c
@@ -31,54 +31,79 @@ void monster_destroy(struct monster_t *monster)
     free(monster);
 }
 
-void monster_act(struct actor_t *actor)
+static void monster_act_move_random(struct actor_t *actor)
 {
     struct monster_t *monster = actor->parent;
+    int delta_x = 0, delta_y = 0;
+    delta_x = TCOD_random_get_int(NULL, -1, 1);
+    delta_y = TCOD_random_get_int(NULL, -1, 1);
 
+    int new_x = monster->drawable->x + delta_x;
+    int new_y = monster->drawable->y + delta_y;
+    if (game_is_walkable(actor->game, new_x, new_y)) {
+        monster->drawable->x = new_x;
+        monster->drawable->y = new_y;
+    }
+
+}
+
+static void monster_act_move_to_target(struct actor_t *actor, struct drawable_t *target)
+{
+    struct monster_t *monster = actor->parent;
     struct drawable_t *this = monster->drawable;
-    int8_t x = this->x, y = this->y;
+    int8_t d_x = this->x - target->x;
+    int8_t d_y = this->y - target->y;
 
-    if (!game_in_fov(actor->game, x, y))
-        return;
+    int delta_x = 0, delta_y = 0;
+    if (d_x > 0) {
+        delta_x = -1;
+    } else if (d_x < 0) {
+        delta_x = 1;
+    }
+    if (d_y > 0) {
+        delta_y = -1;
+    } else if (d_y < 0) {
+        delta_y = 1;
+    }
 
-    int8_t d_x = 0, d_y = 0;
-    struct drawable_t *target = NULL;
+    int new_x = this->x + delta_x;
+    int new_y = this->y + delta_y;
+    if (game_is_walkable(actor->game, new_x, new_y)) {
+        monster->drawable->x = new_x;
+        monster->drawable->y = new_y;
+    }
+
+}
+
+static struct drawable_t *monster_act_find_target(struct actor_t *actor)
+{
+    struct monster_t *monster = actor->parent;
+    struct drawable_t *this = monster->drawable;
+
+    if (!game_in_fov(actor->game, this->x, this->y))
+        return NULL;
+
     for (struct drawable_t *drawable = actor->game->drawable_list; drawable; drawable = drawable->next) {
         if (drawable == this)
             continue;
         if (!drawable->is_attackable)
             continue;
-        d_x = x - drawable->x;
-        d_y = y - drawable->y;
+        int8_t d_x = this->x - drawable->x;
+        int8_t d_y = this->y - drawable->y;
         double distance = sqrt(pow(d_x, 2) + pow(d_y, 2));
-        if (distance < MONSTER_NOTICE_DISTANCE) {
-            target = drawable;
-            break;
-        }
+        if (distance < MONSTER_NOTICE_DISTANCE)
+            return drawable;
     }
 
-    int delta_x = 0, delta_y = 0;
+    return NULL;
+}
+
+void monster_act(struct actor_t *actor)
+{
+    struct drawable_t *target = monster_act_find_target(actor);
     if (target) {
-        if (d_x > 0) {
-            delta_x = -1;
-        } else if (d_x < 0) {
-            delta_x = 1;
-        }
-        if (d_y > 0) {
-            delta_y = -1;
-        } else if (d_y < 0) {
-            delta_y = 1;
-        }
+        monster_act_move_to_target(actor, target);
     } else {
-        delta_x = TCOD_random_get_int(NULL, -1, 1);
-        delta_y = TCOD_random_get_int(NULL, -1, 1);
-    }
-
-    int new_x = monster->drawable->x + delta_x;
-    int new_y = monster->drawable->y + delta_y;
-
-    if (game_is_walkable(actor->game, new_x, new_y)) {
-        monster->drawable->x = new_x;
-        monster->drawable->y = new_y;
+        monster_act_move_random(actor);
     }
 }
